f11f4a00122f2015a6611c19ad5637086430babb
juandalop <juandalop@gmail.com>
2015-03-12 10:08:09 -0500

    login fixed

--- www/js/controllers-login.js
+++ www/js/controllers-login.js
@@ -1,4 +1,4 @@
-moduloControlador.controller('LoginCtrl', function($scope, $rootScope, $ionicLoading, $ionicPopup, $state, $http, $filter, $ionicHistory, Mama, Internet, GA) {
+moduloControlador.controller('LoginCtrl', function($scope, $rootScope, $ionicLoading, $ionicPopup, $state, $http, $filter, $ionicHistory, Mama, Internet, GA, Utilidades) {
 
        //Registro en Analytics      
        GA.trackPage($rootScope.gaPlugin, "Inicio de sesión");
@@ -38,77 +38,57 @@ moduloControlador.controller('LoginCtrl', function($scope, $rootScope, $ionicLoa
             if(Internet.get()){
 
                 $scope.loading =  $ionicLoading.show({
-                    template: 'Iniciando sesión' // <br /><br /> <img style="max-width:50px; max-height:50px;" src="img/loading.gif">'
-                });
+                        template: Utilidades.getPlantillaEspera('Iniciando sesión')
+                    });
 
-            	resultado = Mama.autenticar($scope.datosInicio.cedula, $rootScope, $http, function(success, data){
+            	Mama.autenticar($rootScope.datos.cedula, $rootScope, $http, $filter, function(success, mensajeError, data){
 
                     $ionicLoading.hide();
+                    
+                    if(success){
+                        
+                               $rootScope.campana = {numero: '-', fechaMontajePedido:'-'};
+                                
+                                Mama.getRecordatoriosCampanaOperativa($rootScope.zona, $rootScope, $http, function (success, data){
+                                    if(success){
+                                        $rootScope.campana = {numero: data.listaRecordatorios[0].campagna, fechaMontajePedido:data.listaRecordatorios[0].fecha};
+                                        $rootScope.fechas = data.listaRecordatorios;
+                                    }else{
+                                        
+                                    }
+                                });
 
-                    //Error en la autenticación?
-                    if(data && data.razonRechazo){
+                                Mama.getTrazabilidadPedido($rootScope.datos.cedula, $rootScope, $http, function (success, data){
+                                    if(success){
+                                        $rootScope.pedido = data;
 
-                        if(data.razonRechazo == "El usuario no se encuentra registrado en Antares."){
-                            $scope.mostrarAyuda("Inicio de sesión","Lo sentimos no existe información para esta cédula. Comunícate con la Línea de Atención");
-                        }else{
-                            $scope.mostrarAyuda("Inicio de sesión",data.razonRechazo);
-                        }
-                    }else{
+                                        //Mama.getAgotadosPedido($rootScope.pedido.numeroPedido, $rootScope, $http, function (success, data){
+                                        //if(success){
+                                        //$ionicLoading.hide();
+                                        //  $rootScope.agotados = data;
+                                        //}else{
+                                        //$ionicLoading.hide();
+                                        //$scope.mostrarAyuda("Mi Pedido","En este momento no podemos acceder a tu información");
+                                        //}
+                                        //});
 
-                        //Tipo de usuario recibido?
-                        if(data.tiposUsuarios && data.tiposUsuarios.length > 0 && (data.tiposUsuarios[0] == "1" || data.tiposUsuarios[0] == "3")){
 
-                            //Establecer los datos de resumen de la Mamá
-                            $rootScope.datos.nombre = data.nombreCompleto;
-                            $rootScope.datos.segmento = data.clasificacionValor;
-                            $rootScope.datos.cupo = data.cupo;
-                            $rootScope.datos.saldo = data.saldoBalance;
-                            $rootScope.datos.valorFlexibilizacion = data.valorFlexibilizacion;
-                            $rootScope.zona = data.listaZonas[0];
-
-                            $rootScope.campana = {numero: '-', fechaMontajePedido:'-'};
-
-                            $http.get($rootScope.configuracion.ip_servidores + "/AntaresWebServices/interfaceAntares/getRecordatoriosAntares/" + data.listaZonas[0]).
-                                success(function(data, status, headers, config) {
-                                    $rootScope.campana = {numero: data.listaRecordatorios[0].campagna, fechaMontajePedido:data.listaRecordatorios[0].fecha};
-                                    $rootScope.fechas = data.listaRecordatorios;
-                                }).
-                                error(function(data, status, headers, config) {
-                                    console.log("Error consultando los datos de campaña");
+                                    }else{
+                                        //$ionicLoading.hide();
+                                        //alert("En este momento no podemos acceder a tu información");
+                                    }
                                 });
                                 
-                                
-                            Mama.getTrazabilidadPedido($rootScope.datos.cedula, $rootScope, $http, function (success, data){
-                                if(success){
-                                    $rootScope.pedido = data;
-                                    
-                                    //Mama.getAgotadosPedido($rootScope.pedido.numeroPedido, $rootScope, $http, function (success, data){
-                                    //if(success){
-                                      //$ionicLoading.hide();
-                                    //  $rootScope.agotados = data;
-                                    //}else{
-                                      //$ionicLoading.hide();
-                                    //$scope.mostrarAyuda("Mi Pedido","En este momento no podemos acceder a tu información");
-                                   //}
-                                //});
-                                    
-
-                                }else{
-                                    //$ionicLoading.hide();
-                                    //alert("En este momento no podemos acceder a tu información");
-                                }
-                            });
-
-                            Mama.getPuntos($rootScope.datos.cedula, $rootScope, $http, function (success, data){
-                                if(success){
+                                Mama.getPuntos($rootScope.datos.cedula, $rootScope, $http, function (success, data){
+                                 if(success){
                                     $rootScope.puntos = data;
 
-                                }else{
+                                   }else{
                                     //alert("En este momento no podemos acceder a tu información");
-                                }
-                            });
-
-                            var irABienvenida = !(localStorage && localStorage.nombre);
+                                   }
+                                });
+                                
+                                var irABienvenida = !(localStorage && localStorage.nombre);
 
                             //Almacenar la cédula si hay almacenamiento local
                             if(localStorage ){
@@ -136,23 +116,11 @@ moduloControlador.controller('LoginCtrl', function($scope, $rootScope, $ionicLoa
                             }else{
                                $state.go('app.menu.tabs.home');
                             }
-
+                           
+                            
                         }else{
-
-                            if(data.tiposUsuarios && data.tiposUsuarios.length > 0 && (data.tiposUsuarios[0] == "2")){
-                                $scope.mostrarAyuda("Inicio de sesión","Hola Mamá, te invitamos a montar tu primer pedido para disfurtar de esta Aplicación, para este cuentas con un cupo de " + $filter('currency')(data.cupo, '$', 0));
-                            }else{
-                                if(data.tiposUsuarios){
-                                    $scope.mostrarAyuda("Inicio de sesión","Tu rol no es válido para nuestra Aplicación");
-                                }else{
-                                    $scope.mostrarAyuda("Inicio de sesión","En este momento no podemos consultar tu información");
-                                }
-                            }
-
+                           $scope.mostrarAyuda("Inicio de sesión", mensajeError);
                         }
-
-
-                    }
 
             	});
             	
